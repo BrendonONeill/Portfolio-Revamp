9a716e96fbfbc82defefdbf8bda309c5ce84aafd
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2022-08-10 16:41:16 +0100

    Added project page and design page
    These page will display my projects with information on the projects and the design page will have a few of my designs in a gallery.

---
+++ js/lightbox.js
@@ -0,0 +1,21 @@
+const lightbox = document.querySelector('.lightbox')
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        console.log("image clicked id: " + e )
+        lightbox.classList.add("lightbox-active");
+        const img = document.createElement('img');
+        img.id = "lightbox-img";
+        img.src = image.src;
+        lightbox.appendChild(img);
+    });
+});
+
+lightbox.addEventListener('click', e => {
+    console.log(e.target)
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove("lightbox-active")
+    lightbox.removeChild(lightbox.firstChild)
+})
+
