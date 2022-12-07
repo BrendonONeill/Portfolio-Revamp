539bd4e6850f35d32a1cca70811716034445795c
Brendon <bpjoneill@hotmail.co.uk>
2022-12-07 14:02:10 +0000

    Production stage

---
+++ js/animation.js
@@ -0,0 +1,37 @@
+const textDisplay = document.querySelector("#animatedText");
+const textArray = ["Front-end devloper", "Graphic Designer"];
+let currentText = [" "];
+let j = 0;
+let i = 0;
+let cycleFinished = false;
+function textLoop() {
+  textDisplay.innerHTML = currentText.join("");
+  if (i < textArray.length) {
+    console.log(textArray[i]);
+    if (j < textArray[i].length && cycleFinished === false) {
+      console.log(textArray[i][j]);
+      currentText.push(textArray[i][j]);
+      j++;
+      setTimeout(textLoop, 100);
+    } else if (j > 0 && cycleFinished === true) {
+      console.log(textArray[i][j]);
+      currentText.pop(textArray[i][j]);
+      j--;
+      setTimeout(textLoop, 100);
+    } else if (j == 0 && cycleFinished === true) {
+      cycleFinished = false;
+      i++;
+      setTimeout(textLoop, 100);
+    } else if (j == textArray[i].length) {
+      cycleFinished = true;
+      setTimeout(textLoop, 2000);
+    }
+  } else {
+    i = 0;
+    j = 0;
+    currentText = [];
+    setTimeout(textLoop, 100);
+  }
+}
+
+textLoop();
