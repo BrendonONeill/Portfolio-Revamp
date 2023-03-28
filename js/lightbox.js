2f8971746b377baf438dedbc3db9e65101bd91dc
Brendon <bpjoneill@hotmail.co.uk>
2023-03-28 18:48:40 +0100

    made it more accessible and added dark and light mode

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,9 +1,9 @@
-
 const lightbox = document.querySelector(".lightbox");
-const images = document.querySelectorAll("img");
+const gallery = document.querySelector(".gallery-container");
+const images = gallery.querySelectorAll("img");
+console.log(images);
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
-    console.log("image clicked id: " + e);
     lightbox.classList.add("lightbox-active");
     const img = document.createElement("img");
     img.id = "lightbox-img";
@@ -13,7 +13,6 @@ images.forEach((image) => {
 });
 
 lightbox.addEventListener("click", (e) => {
-  console.log(e.target);
   if (e.target !== e.currentTarget) return;
   lightbox.classList.remove("lightbox-active");
   lightbox.removeChild(lightbox.firstChild);
